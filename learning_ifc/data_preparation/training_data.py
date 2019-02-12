78412616ecc47505f58d8ee7111d62184f19260d
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2019-02-12 12:14:30 +0100

    filtered training.

--- learning_ifc/data_preparation/training_data.py
+++ learning_ifc/data_preparation/training_data.py
@@ -5,6 +5,8 @@ with 1000 cells per class.
 import sys
 from itertools import islice
 
+from matplotlib import pyplot as plt
+
 from skimage.external.tifffile import TiffWriter
 from pims import ND2_Reader
 
@@ -17,10 +19,29 @@ with ND2_Reader(path) as frames:
   frames.bundle_axes = 'xy'
   fr = frames[21] # maximally in-focus frame
 
-  yeasts = [
+  yeasts_raw = [
     (slice(int(x), int(x) + 128), slice(int(y), int(y) + 128))
-    for _, (x, y) in islice(MANAArray(fr, size=(128, 128)), 1000)
+    for _, (x, y) in MANAArray(fr, size=(128, 128))
   ]
+
+  total = 0
+  yeasts = []
+  accept = [False]
+  def press(event):
+    accept[0] = event.key == "enter"
+  for cell, (xs, ys) in enumerate(yeasts_raw):
+    if total >= 1000:
+      break
+    fig, ax = plt.subplots()
+    ax.imshow(fr[xs, ys])
+
+    fig.canvas.mpl_connect('key_press_event', press)
+
+    plt.show(block=False)
+    plt.waitforbuttonpress()
+    if accept:
+      yeasts.append((xs, ys))
+      total += 1
 
   for level, frame in enumerate(frames):
     for cell, (xs, ys) in enumerate(yeasts):
